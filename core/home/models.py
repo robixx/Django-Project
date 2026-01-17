66219f606c5b1bba725a69d6466bcdef58a0da75
Md. Shoriful Islam <shorifulislam776@gmail.com>
2026-01-17 18:57:40 +0600

    working javascript

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    UserId = models.AutoField(primary_key=True)
+    UserName = models.CharField(max_length=250, null=True, blank=True)
+    MobileNumber = models.CharField(max_length=50, null=True, blank=True)
+    Password = models.CharField(max_length=50)
+    Gender = models.CharField(max_length=50)   
+    Status = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        managed = False              # 🔥 VERY IMPORTANT
+        db_table = 'app_Users'       # exact table name
--- core/home/views.py
+++ core/home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
-
+from django.contrib import messages
+from .models import UserProfile
+from django.contrib.auth.hashers import make_password
 def home(request):
     return render(request,'index.html')
 
@@ -9,4 +11,37 @@ def usapage(request):
     return render(request,'pages/usa.html')
 
 def userList(request):
-    return render(request,'pages/usercreate.html')
+    print("METHOD:", request.method)
+
+    if request.method == "POST":
+        print("POST DATA:", request.POST)
+
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        mobile_no = request.POST.get("mobileNo")
+        gender = request.POST.get("gender")
+        password = request.POST.get("password")
+        password1 = request.POST.get("password1")
+
+        print("USERNAME:", username)
+
+        if password != password1:
+            messages.error(request, "Passwords do not match")
+            return redirect("userList")
+
+        try:
+            UserProfile.objects.create(
+                UserName=username,               
+                MobileNumber=mobile_no,
+                Gender=gender,
+                Status=1,
+                Password=make_password(password)
+            )
+            print("USER SAVED")
+        except Exception as e:
+            print("ERROR:", e)
+
+        messages.success(request, "User created successfully")
+        return redirect("userList")
+
+    return render(request, "pages/usercreate.html")
